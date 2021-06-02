7ae0d37d47a541fc3538424f135eff787423fc83
Jacob Arellano <jmm.arellano@gmail.com>
2021-06-02 10:40:53 -0600

    Add route for tea subscription

---
+++ app/controllers/api/v1/user_subcriptions_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::UserSubcriptionsController < ApplicationController
+ def create
+   require "pry"; binding.pry
+
+ end
+end
--- spec/requests/api/v1/add_tea_subscription_request_spec.rb
+++ spec/requests/api/v1/add_tea_subscription_request_spec.rb
@@ -4,10 +4,21 @@ RSpec.describe 'Create a pet request' do
   describe 'happy path' do
     before :each do
       @user = create(:user)
+      @subscription = create(:subscription)
     end
 
     it "can add a subscription to an existing user" do
-      
+      subscription_info = {
+                            user_id: @user.id,
+                            subscription_id: @subscription.id,
+                            active: true
+                          }
+
+      headers = {"CONTENT_TYPE" => "application/json",
+                 "ACCEPT"       => "application/json"}
+
+      post '/api/v1/subscription', headers: headers, params: subscription_info.to_json
+
     end
   end
 end
