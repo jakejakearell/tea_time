608456bdd1ffb35600d50bb89db86e2687e7e828
Jacob Arellano <jmm.arellano@gmail.com>
2021-06-03 11:58:24 -0600

    Add assertions for user subscriptions

--- spec/requests/api/v1/get_all_user_subscriptions_spec.rb
+++ spec/requests/api/v1/get_all_user_subscriptions_spec.rb
@@ -11,26 +11,53 @@ RSpec.describe 'User subscription' do
     end
 
     it "can add a subscription to an existing user" do
-      require "pry"; binding.pry
+
       get "/api/v1/user/#{@user.id}/subscriptions"
 
       expect(response).to be_successful
-      expect(response.status).to eq(201)
+      expect(response.status).to eq(200)
       expect(response.content_type).to eq("application/json")
 
       subscription_json = JSON.parse(response.body, symbolize_names: true)
+      expect(subscription_json).to be_a(Hash)
+      expect(subscription_json[:data]).to be_a(Array)
+      expect(subscription_json[:data].count).to eq(10)
 
+      subscription_json[:data].each do |subscription|
+        expect(subscription.keys).to eq([:id, :type, :attributes])
+        expect(subscription[:id]).to be_a(String)
+        expect(subscription[:type]).to eq('subscriptions')
+        expect(subscription[:attributes].count).to eq(3)
+        expect(subscription[:attributes].keys).to eq([:title, :price, :frequency])
+        expect(subscription[:attributes][:title]).to be_a(String)
+        expect(subscription[:attributes][:price]).to be_a(Float)
+        expect(subscription[:attributes][:frequency]).to be_a(String)
+      end
     end
   end
 
   describe 'sad path' do
 
     it "will give error if user doesn't exist" do
+      get "/api/v1/user/420/subscriptions"
+      expect(response.status).to eq(404)
 
+      message = JSON.parse(response.body, symbolize_names: true)
+      expect(message[:error]).to eq("No such user")
     end
 
     it "will give return an empty array if user has no subscriptions" do
+      user = create(:user)
+      get "/api/v1/user/#{user.id}/subscriptions"
 
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq("application/json")
+
+      subscription_json = JSON.parse(response.body, symbolize_names: true)
+      expect(subscription_json).to be_a(Hash)
+      expect(subscription_json[:data]).to be_a(Array)
+      expect(subscription_json[:data].count).to eq(0)
     end
   end
 end
