0239542943e6f4b0efae093d135fbf91002c72d0
Jacob Arellano <jmm.arellano@gmail.com>
2021-06-03 10:50:38 -0600

    Route and controller action for updating user subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'subscription', to: 'user_subcriptions#create'
+      put 'subscription', to: 'user_subcriptions#update'
     end
   end
 end
